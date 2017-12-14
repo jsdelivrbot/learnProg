1bc0ebfdb9da7d96c616b6926949a1812142481b
Tino Romero <romerotino@gmail.com>
2017-12-14 00:41:07 -0700

    continue reduxmiddleware

--- React/ReduxMiddleware/src/actions/index.js
+++ React/ReduxMiddleware/src/actions/index.js
@@ -1,1 +1,21 @@
+import axios from 'axios';
+
 const API_KEY = '7e8d544f194cf77ea8efcdb86955a1e5';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+
+
+/*
+http://samples.openweathermap.org/data/2.5/forecast?appid=7e8d544f194cf77ea8efcdb86955a1e5&q=Chicago,us
+ */

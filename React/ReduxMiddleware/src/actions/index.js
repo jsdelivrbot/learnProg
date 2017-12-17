441c84968d3e1b37c340228abf74ef19c681ac7c
Tino Romero <romerotino@gmail.com>
2017-12-17 13:55:31 -0700

    continure ReduxMiddleware

--- React/ReduxMiddleware/src/actions/index.js
+++ React/ReduxMiddleware/src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
 const API_KEY = '7e8d544f194cf77ea8efcdb86955a1e5';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
@@ -17,5 +19,5 @@ export function fetchWeather(city) {
 
 
 /*
-http://samples.openweathermap.org/data/2.5/forecast?appid=7e8d544f194cf77ea8efcdb86955a1e5&q=Chicago,us
- */
+http://api.openweathermap.org/data/2.5/forecast?appid=7e8d544f194cf77ea8efcdb86955a1e5&q=Chicago,us
+*/
--- React/ReduxMiddleware/src/index.js
+++ React/ReduxMiddleware/src/index.js
@@ -1,4 +1,4 @@
-// 57
+// 58
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';

e04579e7802f3bd3a8a12f4b75ebf75b0cec973d
juventinoromero <jromero@nearsoft.com>
2017-12-18 21:17:46 -0700

    ReduxMiddleware - add chart component

--- React/ReduxMiddleware/src/actions/index.js
+++ React/ReduxMiddleware/src/actions/index.js
@@ -9,10 +9,12 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
-  console.log('Request:', request);
-
   return {
     type: FETCH_WEATHER,
     payload: request
   };
 }
+
+/*
+http://api.openweathermap.org/data/2.5/forecast?appid=7e8d544f194cf77ea8efcdb86955a1e5&q=Chicago,us
+ */

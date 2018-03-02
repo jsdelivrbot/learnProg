22b63e1321d8fde4696ffa798c17ae0e597534f5
Tino Romero <romerotino@gmail.com>
2018-03-02 13:31:24 -0700

    React testing - finish step 1 of testing environment setup

--- React/testing/test/test_helper.js
+++ React/testing/test/test_helper.js
@@ -1,8 +1,12 @@
 import jsdom from 'jsdom';
+import jquery from 'jquery';
 
 // set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+// tell jquery not to try to reach out the dom and to be responsible just of that piece of dome just created.
+// From here on out, everywhere else where we need to use jquery in the testing setup we are going to use this modified version of jquery ($)
+const $ = jquery(global.window);
 
 // build renderComponent helper that should render a given react class
 
